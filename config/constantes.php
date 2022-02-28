5d92e2258efc95c1893a0ffa26a3d700a69d7368
Adama@455 <1994adamandiaye@gmail.com>
2022-02-28 10:06:02 +0000

    definition des constantes du projet

---
+++ config/constantes.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Chemin sur le dossier racine du projet:
+ */
+define("ROOT",str_replace("public".DIRECTORY_SEPARATOR."index.php","",$_SERVER["SCRIPT_FILENAME"]));
+
+/**
+ * Chemin sur le dossier src qui contien les controllers et les modeles
+ */
+define("PATH_SRC",ROOT."src".DIRECTORY_SEPARATOR);
+
+/**
+ * Chemin sur les Templates du projet:
+ */
+define("PATH_VIEWS",ROOT."templates".DIRECTORY_SEPARATOR);
+
+/**
+ * Chemin qui nous mene vers le fichier json du projet:
+ */
+define("PATH_DB",ROOT."data".DIRECTORY_SEPARATOR."db.json");
+
+/**
+ * Requett POST et GET:
+ */
+define("WEB_ROOT","http://localhost:8002/");
