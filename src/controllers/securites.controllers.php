8fbe096be3e705a9e6ccabc1583f14ac3ee4b399
Adama@455 <1994adamandiaye@gmail.com>
2022-02-28 12:37:27 +0000

    creation du controller securites

---
+++ src/controllers/securites.controllers.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD']=="POST") {
+    if (isset($_REQUEST['action'])) {
+        if ($_REQUEST['action']=="connexion") {
+           echo "traiter le formulaire de connexion";   
+        }
+      
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD']=="GET") {
+    if (isset($_REQUEST['action'])) {
+        if ($_REQUEST['action']=="connexion") {
+           echo "Charger la page de connexion";   
+        }
+      
+    }
+}
