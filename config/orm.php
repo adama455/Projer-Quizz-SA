e26d9f32b7fe373f2e8681fb03a2d6377ad6221e
Adama@455 <1994adamandiaye@gmail.com>
2022-02-28 12:31:00 +0000

    creation orm

--- config/orm.php
+++ config/orm.php
@@ -1,1 +1,15 @@
 
+<?php
+///REcuperation des donnees du fichier
+function json_to_array(string $key):array{
+    $datajson = (file_get_contents(PATH_DB));
+    $data = json_decode($json,true);
+    return $data[$key];
+}
+
+//Enregistrement et Mis à jour des donnees du fichier:
+function array_to_json(string $key,array $data):string{
+    $datajson = json_encode($json,true);
+    return[$key];
+}
+ 
