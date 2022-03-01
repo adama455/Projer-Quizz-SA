dd4a681e12dac333f9334a87bec0e72482abd8b3
Adama@455 <1994adamandiaye@gmail.com>
2022-03-01 08:42:36 +0000

    verification de l existance et stockage des donnees dans la session

--- src/controllers/securites.controllers.php
+++ src/controllers/securites.controllers.php
@@ -1,18 +1,61 @@
 <?php
+
+require_once(PATH_SRC."modeles".DIRECTORY_SEPARATOR."user.model.php");
 if ($_SERVER['REQUEST_METHOD']=="POST") {
     if (isset($_REQUEST['action'])) {
         if ($_REQUEST['action']=="connexion") {
-           echo "traiter le formulaire de connexion";   
+          //Recuperation du login et Mot de passe
+            $login=$_POST['login'];
+            $password=$_POST['password'];
+            //Appel de la fonction connexion 
+            connexion($login,$password); 
+        }
+      
+    }
+} 
+
+if ($_SERVER['REQUEST_METHOD']=="GET") {
+    if (isset($_REQUEST['action'])) {
+        if ($_REQUEST['action']=="connexion") {
+           require_once(PATH_VIEWS."securites".DIRECTORY_SEPARATOR."connexion.html.php");   
         }
       
     }
 }
 
-if ($_SERVER['REQUEST_METHOD']=="GET") {
-    if (isset($_REQUEST['action'])) {
-        if ($_REQUEST['action']=="connexion") {
-           echo "Charger la page de connexion";   
-        }
-      
+/**
+ * Fonction de Connexion
+ */
+function connexion(string $login, string $password):void{
+    $error=[];
+    champ_obligatoire('login',$loing,$error,"login obligatoire");
+    if (count($error)==0) {
+        valid_email('loing', $login,$error);
     }
+
+    champ_obligatoire('password',$password,$error,"password obligatoire");
+    if (count($error)==0) {
+        //Appel de la fonction du modeles
+        $users=find_user_login_password($login,$password);
+        
+        if (count($users)!=0) {
+            //Utilisateur Exist
+            $_SESSION[KEY_USERS_CONNECT]=$users;
+            header('location:'.WEB_ROOT."?controller=user&action=accueil");
+            exit();
+        }else {
+            //Utilisateur n'Exist pas
+            $error["connexion"]="Login ou Mot de passe Incorrect!";
+            $_SESSION[KEY_ERROR]=$error;
+            header('location:'.WEB_ROOT);
+            exit();
+        }
+        
+    // valid_password('password', $password,$error);
+    }else{
+        //Erreur de validation
+        $_SESSION[KEY_ERROR]=$error;
+        header('location:'.WEB_ROOT);
+        exit();
+    }    
 }
