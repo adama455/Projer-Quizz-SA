3f58b3c7ff9a9e1e1a4c798bdc9a31c045b2ae74
Adama@455 <1994adamandiaye@gmail.com>
2022-03-02 09:35:34 +0000

    complement du controllers pour la connexion

--- src/controllers/securites.controllers.php
+++ src/controllers/securites.controllers.php
@@ -1,23 +1,30 @@
 <?php
 
 require_once(PATH_SRC."modeles".DIRECTORY_SEPARATOR."user.model.php");
-if ($_SERVER['REQUEST_METHOD']=="POST") {
-    if (isset($_REQUEST['action'])) {
-        if ($_REQUEST['action']=="connexion") {
-          //Recuperation du login et Mot de passe
+
+if ($_SERVER['REQUEST_METHOD']=="POST") {   
+    if (isset($_POST['action'])){
+        
+        if ($_POST['action']=="connexion") 
+        {
+            // die("Vous etes sur la page" );
+            //Recuperation du login et Mot de passe
             $login=$_POST['login'];
             $password=$_POST['password'];
             //Appel de la fonction connexion 
             connexion($login,$password); 
-        }
-      
-    }
-} 
+        }  
+        
+    } 
+}
+
 
 if ($_SERVER['REQUEST_METHOD']=="GET") {
     if (isset($_REQUEST['action'])) {
         if ($_REQUEST['action']=="connexion") {
            require_once(PATH_VIEWS."securites".DIRECTORY_SEPARATOR."connexion.html.php");   
+        }elseif($_REQUEST['action']=="deconnexion") {
+            logout();
         }
       
     }
@@ -28,7 +35,8 @@ if ($_SERVER['REQUEST_METHOD']=="GET") {
  */
 function connexion(string $login, string $password):void{
     $error=[];
-    champ_obligatoire('login',$loing,$error,"login obligatoire");
+    champ_obligatoire('login',$login,$error,"login obligatoire");
+
     if (count($error)==0) {
         valid_email('loing', $login,$error);
     }
@@ -36,18 +44,18 @@ function connexion(string $login, string $password):void{
     champ_obligatoire('password',$password,$error,"password obligatoire");
     if (count($error)==0) {
         //Appel de la fonction du modeles
-        $users=find_user_login_password($login,$password);
-        
+        $users=find_user_loing_password($login,$password);
+        // var_dump($users);
         if (count($users)!=0) {
             //Utilisateur Exist
             $_SESSION[KEY_USERS_CONNECT]=$users;
-            header('location:'.WEB_ROOT."?controller=user&action=accueil");
+            header('location:'.WEB_ROOT."?controllers=users&action=accueil");
             exit();
         }else {
             //Utilisateur n'Exist pas
             $error["connexion"]="Login ou Mot de passe Incorrect!";
             $_SESSION[KEY_ERROR]=$error;
-            header('location:'.WEB_ROOT);
+            header('location:'.WEB_ROOT."?controllers=securites&action=connexion");
             exit();
         }
         
@@ -55,7 +63,11 @@ function connexion(string $login, string $password):void{
     }else{
         //Erreur de validation
         $_SESSION[KEY_ERROR]=$error;
-        header('location:'.WEB_ROOT);
+        header('location:'.WEB_ROOT."?controllers=securites&action=connexion");
         exit();
     }    
+}
+function logout(){
+    session_destroy();
+    header('location:'.WEB_ROOT."?controller=securites&action=connexion");
 }
--- src/controllers/users.controllers.php
+++ src/controllers/users.controllers.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once(PATH_SRC."modeles".DIRECTORY_SEPARATOR."user.model.php");
+
 if ($_SERVER['REQUEST_METHOD']=="POST") {
     if (isset($_REQUEST['action'])) {
         if ($_REQUEST['action']=="connexion") {
@@ -10,8 +13,43 @@ if ($_SERVER['REQUEST_METHOD']=="POST") {
 
 if ($_SERVER['REQUEST_METHOD']=="GET") {
     if (isset($_REQUEST['action'])) {
+        if (!is_connect()) {
+            header('location:'.WEB_ROOT."?controller=securites&action=connexion");
+            exit();
+        }
         if ($_REQUEST['action']=="accueil") {
+            
+            if (is_admin()) {
+               lister_joueur();
+            }elseif (is_joueur()) {
+                jeu();
+            }
+            
             require_once(PATH_VIEWS."users".DIRECTORY_SEPARATOR."accueil.html.php");              
-        }  
+        }elseif($_REQUEST['action']=="liste.joueur") {
+            lister_joueur();
+        }
     }
+}
+
+function lister_joueur() {
+    // Appel du Model
+    ob_start();
+        $data = find_users("ROLE_JOUEUR");
+        require_once(PATH_VIEWS."users".DIRECTORY_SEPARATOR."liste.joueur.html.php");   
+    $content_for_views=ob_get_clean();  
+    
+    require_once(PATH_VIEWS."users".DIRECTORY_SEPARATOR."accueil.html.php");              
+    
+
+}
+
+// Fonction Jeu
+function jeu() {
+    ob_start();
+        require_once(PATH_VIEWS."users".DIRECTORY_SEPARATOR."jeux.html.php");   
+    $content_for_views=ob_get_clean();  
+    
+    require_once(PATH_VIEWS."users".DIRECTORY_SEPARATOR."accueil.html.php");              
+    
 }
