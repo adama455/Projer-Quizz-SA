74612d6168d302930fa9105352cdbca1677a1a24
Adama@455 <1994adamandiaye@gmail.com>
2022-03-02 09:42:42 +0000

    Integration de la views header

---
+++ templates/include/header.inc.html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>QUIZZ-APP</title>
+        <link rel="stylesheet" href="<?= WEB_ROOT."css".DIRECTORY_SEPARATOR."style.accueil.css"?>" media="screen" type="text/css" />
+        <link rel="stylesheet" href="<?= WEB_ROOT."css".DIRECTORY_SEPARATOR."style.connexion.css"?>" media="screen" type="text/css" />
+
+    </head>
+<body>
